57446872bafd574d20a4b8aff6ccb997dd099023
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-01-08 11:47:19 +0700

    feat: Admin list movie

--- resources/js/Components/InputLabel.jsx
+++ resources/js/Components/InputLabel.jsx
@@ -1,6 +1,9 @@
 export default function InputLabel({ forInput, value, className, children }) {
     return (
-        <label htmlFor={forInput} className={`text-base block mb-2` + className}>
+        <label
+            htmlFor={forInput}
+            className={`text-base font-bold block mb-2` + className}
+        >
             {value ? value : children}
         </label>
     );
